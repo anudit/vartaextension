4553a9ef8deff8178badff0e17cad52e9322f915
Anudit Nagar <nagaranudit@gmail.com>
2021-09-25 21:11:21 +0530

    improvements

--- background.js
+++ background.js
@@ -10,3 +10,24 @@ chrome.action.onClicked.addListener((tab) => {
     }
 
 });
+
+chrome.commands.onCommand.addListener((command) => {
+    console.log(`Command "${command}" triggered`);
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+        var activeTab = tabs[0];
+        try {
+            let url = new URL(activeTab.url);
+            let link = url.origin + url.pathname;
+            console.log(btoa(link), link);
+            chrome.tabs.create({active: true, url: `https://theconvo.space/site/${btoa(link)}`});
+        } catch (error) {
+            console.log(error);
+        }
+
+    });
+});
+
+chrome.contextMenus.onClicked.addListener((data)=>{
+    console.log(data);
+});
